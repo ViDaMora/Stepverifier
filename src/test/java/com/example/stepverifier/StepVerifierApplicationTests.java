b3290de748da01ab482f69a3276ee5b10ffa5728
Victor Mora <vdaniel.mora@udea.edu.co>
2021-06-24 21:57:25 -0500

    Pruebas unitarias a servicios asincronos Flux y Mono

---
+++ src/test/java/com/example/stepverifier/StepVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepVerifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
